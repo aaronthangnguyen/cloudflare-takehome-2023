e0a7bd8f6b243dfb97b054e7e7f15d335b8e4aea
Aaron Nguyen <me@aaronnguyen.dev>
2023-10-26 22:55:40 -0400

    Add storage APIs

---
+++ workers/src/kv/employees.ts
@@ -0,0 +1,39 @@
+import { v4 as uuidv4 } from 'uuid';
+
+interface Employee {
+	name: string;
+	department: string;
+	salary: number;
+	office: string;
+	isManager: boolean;
+	skills: string[];
+}
+
+const getEmployees = async (kvStore: KVNamespace) => {
+	const value = await kvStore.list<string>();
+	const employeePromises: Promise<Employee | null>[] = value.keys.map(async ({ name: id }) => {
+		const value = await kvStore.get<Employee>(id, { type: 'json' });
+		return value;
+	});
+
+	const filteredEmployeePromises: Promise<Employee>[] = employeePromises
+		.filter((promise): promise is Promise<Employee> => promise !== null)
+		.map((promise) => promise as Promise<Employee>);
+
+	return await Promise.all(filteredEmployeePromises);
+};
+
+const postEmployees = async (employees: Employee[], kvStore: KVNamespace): Promise<void> => {
+	try {
+		const employeePromises = employees.map((employee) => {
+			const employeeId = uuidv4();
+			return kvStore.put(employeeId, JSON.stringify(employee));
+		});
+		await Promise.all(employeePromises);
+	} catch (error) {
+		console.error('Error posting employees:', error);
+		throw error;
+	}
+};
+
+export { type Employee, getEmployees, postEmployees };
